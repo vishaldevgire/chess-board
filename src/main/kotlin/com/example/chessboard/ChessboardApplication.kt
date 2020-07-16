0a98961319fe86a84e1cd005d65a97b53ff81afb
vishaldevgire <vishaldevgire@gmail.com>
2020-07-17 01:15:48 +0530

    Add basic gradle project structure

---
+++ src/main/kotlin/com/example/chessboard/ChessboardApplication.kt
@@ -0,0 +1,11 @@
+package com.example.chessboard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChessboardApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChessboardApplication>(*args)
+}
