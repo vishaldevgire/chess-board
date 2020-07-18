c588fb0fa4e4dad11a9f7c5c99c6512b7c6c5500
vishaldevgire <vishaldevgire@gmail.com>
2020-07-19 00:19:51 +0530

    Add logic to handle input and print moves for specified chess piece

--- src/main/kotlin/com/example/chessboard/ChessboardApplication.kt
+++ src/main/kotlin/com/example/chessboard/ChessboardApplication.kt
@@ -1,12 +1,38 @@
 package com.example.chessboard
 
-import com.example.chessboard.pieces.Knight
+import com.example.chessboard.pieces.*
 
-fun main() {
+fun main(args: Array<String>) {
+    val factory = mapOf<String, () -> Piece>(
+            "King"   to { King() },
+            "Horse"  to { Knight() },
+            "Pawn"   to { Pawn() },
+            "Queen"  to { Queen() },
+            "Bishop" to { Bishop() },
+            "Rook"   to { Rook() }
+    )
+
+	if (args.size < 2) {
+		println("Usage: <jar> <King|Queen|Bishop|Horse|Rook|Pawn> <board_position>")
+		println("  -example: <jar> King D4")
+		return
+	}
+
+	val pieceName = args[0]
+	val position = args[1]
+
+	if (factory[pieceName] == null) {
+		println("Error: Invalid chess piece specified.")
+		println("Usage: <jar> <King|Queen|Bishop|Horse|Rook|Pawn> <board_position>")
+		println("  -example: <jar> King D4")
+		return
+	}
+
 	val chessboard = Chessboard()
-	val knight = Knight()
+	val piece = factory[pieceName]!!.invoke()
 
-	chessboard.place("d5", knight)
+	chessboard.place(position, piece)
 
-	print("Moves: ${knight.moves()}")
+	println(piece.moves())
 }
+
